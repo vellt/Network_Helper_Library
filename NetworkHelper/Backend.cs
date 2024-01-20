5aad1bee98c4c1c0a2881cf7a6a44fdd91f729e1
Szanto Benjamin <szantobenjamin97@gmail.com>
2024-01-20 21:44:37 +0100

    v0.0.3

---
+++ NetworkHelper/Backend.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkHelper
+{
+    public class Response
+    {
+        public Response(string message, bool error, StatusCode statusCode)
+        {
+            Message = message;
+            StatusCode = statusCode;
+        }
+        public StatusCode StatusCode { get; }
+        public string Message { get; }
+        public List<T> ToList<T>()
+        {
+            if (StatusCode != StatusCode.OK)
+            {
+                return new List<T>(); // hiba esetén üres listával tér vissza
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<List<T>>(Message);
+            }
+        }
+    }
+
+    public class RequestBuilder
+    {
+        WebRequest request;
+        public RequestBuilder(string method, string from)
+        {
+            var request = WebRequest.Create(from);
+            request.Method = method;
+            this.request = request;
+        }
+
+        public RequestBuilder Body<T>(T body)
+        {
+            request.ContentType = "application/json";
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                Dictionary<string, string> dictionary = new Dictionary<string, string>();
+                PropertyInfo[] properties = body.GetType().GetProperties();
+                foreach (PropertyInfo property in properties)
+                {
+                    string propertyName = property.Name;
+                    object propertyValue = property.GetValue(body);
+                    if (propertyValue != null)
+                    {
+                        dictionary.Add(propertyName, propertyValue.ToString());
+                    }
+                }
+                streamWriter.Write(JsonConvert.SerializeObject(dictionary));
+            }
+            return this;
+        }
+
+        public Response Send()
+        {
+            var response = request.GetResponse();
+            if (response != null && ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK)
+            {
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string json = streamReader.ReadToEnd();
+                    return new Response(message: json, error: false, statusCode: (StatusCode)((HttpWebResponse)response).StatusCode);
+                }
+            }
+            else
+            {
+                return new Response(message: ((HttpWebResponse)response).StatusCode.ToString(), error: true, statusCode: (StatusCode)((HttpWebResponse)response).StatusCode);
+            }
+        }
+    }
+
+    public static class Backend
+    {
+        public static RequestBuilder GET(string from) => new RequestBuilder(nameof(GET), from);
+
+        public static RequestBuilder POST(string from) => new RequestBuilder(nameof(POST), from);
+
+        public static RequestBuilder PUT(string from) => new RequestBuilder(nameof(PUT), from);
+
+        public static RequestBuilder DELETE(string from) => new RequestBuilder(nameof(DELETE), from);
+    }
+}
+
+
