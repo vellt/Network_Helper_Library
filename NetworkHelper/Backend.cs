ac324adcb3713fd332d536638429e8486b8f7831
Szanto Benjamin <szantobenjamin97@gmail.com>
2024-01-21 17:07:14 +0100

    v0.0.4

--- NetworkHelper/Backend.cs
+++ NetworkHelper/Backend.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
+using System.Net.Http;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,49 +37,110 @@ namespace NetworkHelper
     public class RequestBuilder
     {
         WebRequest request;
+
+        MultipartFormDataContent formData;
+        HttpClient httpClient;
+        FileStream fileStream;
+        string url;
+        string method;
         public RequestBuilder(string method, string from)
         {
             var request = WebRequest.Create(from);
             request.Method = method;
             this.request = request;
+            this.url = from;
+            this.method = method;
         }
 
-        public RequestBuilder Body<T>(T body)
+        public RequestBuilder Body<T>(T body, Expression<Func<T, object>> fileProperty = null)
         {
-            request.ContentType = "application/json";
-            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            if (fileProperty == null)
             {
-                Dictionary<string, string> dictionary = new Dictionary<string, string>();
-                PropertyInfo[] properties = body.GetType().GetProperties();
-                foreach (PropertyInfo property in properties)
+                request.ContentType = "application/json";
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
                 {
-                    string propertyName = property.Name;
-                    object propertyValue = property.GetValue(body);
-                    if (propertyValue != null)
+                    Dictionary<string, string> dictionary = new Dictionary<string, string>();
+                    PropertyInfo[] properties = body.GetType().GetProperties();
+                    foreach (PropertyInfo property in properties)
                     {
-                        dictionary.Add(propertyName, propertyValue.ToString());
+                        string propertyName = property.Name;
+                        object propertyValue = property.GetValue(body);
+                        if (propertyValue != null)
+                        {
+                            dictionary.Add(propertyName, propertyValue.ToString());
+                        }
+                    }
+                    streamWriter.Write(JsonConvert.SerializeObject(dictionary));
+                }
+            }
+            else
+            {
+                var compiledFileProperty = fileProperty.Compile();
+                MemberExpression memberExpression = (MemberExpression)fileProperty.Body;
+                string filePath = compiledFileProperty(body).ToString();
+                httpClient = new HttpClient();
+                {
+                    formData = new MultipartFormDataContent();
+                    {
+                        fileStream = File.OpenRead(filePath);
+                        {
+                            var streamContent = new StreamContent(fileStream);
+
+                            formData.Add(streamContent, memberExpression.Member.Name, filePath.Split('\\')[filePath.Split('\\').Length - 1]);
+
+                            PropertyInfo[] properties = body.GetType().GetProperties();
+                            foreach (PropertyInfo property in properties)
+                            {
+                                string propertyName = property.Name;
+                                object propertyValue = property.GetValue(body);
+                                if (propertyValue != null && propertyName != memberExpression.Member.Name)
+                                {
+                                    formData.Add(new StringContent(propertyValue.ToString()), propertyName);
+                                }
+                            }
+                        }
+
                     }
                 }
-                streamWriter.Write(JsonConvert.SerializeObject(dictionary));
+
             }
             return this;
         }
 
         public Response Send()
         {
-            var response = request.GetResponse();
-            if (response != null && ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK)
+            if (formData != null)
             {
-                using (var streamReader = new StreamReader(response.GetResponseStream()))
+
+                var response = (method == "POST") ? httpClient.PostAsync(url, formData).Result : (method == "PUT") ? httpClient.PutAsync(url, formData).Result : null;
+
+                if (response != null && response.IsSuccessStatusCode)
                 {
-                    string json = streamReader.ReadToEnd();
-                    return new Response(json, (StatusCode)((HttpWebResponse)response).StatusCode);
+                    string json = response.Content.ReadAsStringAsync().Result;
+                    return new Response(message: json, statusCode: (StatusCode)response.StatusCode);
+                }
+                else
+                {
+                    return new Response(message: ((StatusCode)response.StatusCode).ToString(), statusCode: (StatusCode)response.StatusCode);
                 }
             }
             else
             {
-                return new Response(((HttpWebResponse)response).StatusCode.ToString(), (StatusCode)((HttpWebResponse)response).StatusCode);
+                var response = request.GetResponse();
+                if (response != null && ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK)
+                {
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string json = streamReader.ReadToEnd();
+                        return new Response(message: json, statusCode: (StatusCode)((HttpWebResponse)response).StatusCode);
+                    }
+                }
+                else
+                {
+                    return new Response(message: ((HttpWebResponse)response).StatusCode.ToString(), statusCode: (StatusCode)((HttpWebResponse)response).StatusCode);
+                }
             }
+
         }
     }
 
